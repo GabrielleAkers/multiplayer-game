05a855fe5e6b343bf51febc30e2ccd1191d68ce1
Gabrielle Akers (She/Her) <46634587+GabrielleAkers@users.noreply.github.com>
2024-07-19 15:01:04 -0400

    .

--- out/common.mjs
+++
@@ -1,7 +0,0 @@
-export const SERVER_PORT = 7070;
-export const CANVAS_SCALE = 100;
-export const CANVAS_WIDTH = 16 * CANVAS_SCALE;
-export const CANVAS_HEIGHT = 9 * CANVAS_SCALE;
-export const PLAYER_RADIUS = 10;
-export const PLAYER_SPEED = 200;
-//# sourceMappingURL=common.mjs.map
